f5a2b671e9235eb25185ee1de928fd325aab8777
Yehuda Ringler <yrappdev@gmail.com>
2020-07-14 20:22:54 -0400

    get duration of most of rest - others probably bad

--- audiolength/get_duration.js
+++ audiolength/get_duration.js
@@ -31,12 +31,12 @@ for (const source of sources) {
 fs.writeFileSync('duration.json', JSON.stringify(sourceMap));
 
 function getDuration(source) {
-	let duration15 = getDurationFromPartial(source, 1500);
-	let duration30 = getDurationFromPartial(source, 3000);
+	// let duration15 = getDurationFromPartial(source, 1500);
+	// let duration30 = getDurationFromPartial(source, 3000);
 
-	if (duration15 && duration15 == duration30) {
-		return duration30;
-	}
+	// if (duration15 && duration15 == duration30) {
+	// 	return duration30;
+	// }
 
 	return getDurationFromPartial(source);
 }
@@ -46,11 +46,11 @@ function getDurationFromPartial(source, bytes) {
 
 	try {
 		child_process.execSync(`curl -s ${rangeArguments} "${encodeURI(source)}" --output tmp.mp3 > null.json`);
-		const durationCommand = `mediainfo --Output="Audio;%FileName% %Duration%" tmp.mp3`;
+		const durationCommand = `mp3info -p "%S" tmp.mp3`;
 		const duration = child_process.execSync(durationCommand, {
 			encoding: 'utf8'
 		});
-		return +duration.trim();
+		return (+duration.trim()) * 1000;
 	} catch (ex) {
 		return 0;
 	}
